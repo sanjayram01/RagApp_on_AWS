afa56467c022138ec98c9d2903c21c0f21e0e034
Kennywu <jdlow@live.cn>
2024-11-04 15:47:50 +0800

    feat: support for visualizing citation results (via embeddings)
    Signed-off-by: Kennywu <jdlow@live.cn>

--- libs/kotaemon/kotaemon/indices/qa/__init__.py
+++ libs/kotaemon/kotaemon/indices/qa/__init__.py
@@ -1,7 +1,9 @@
 from .citation import CitationPipeline
 from .text_based import CitationQAPipeline
+from .visualize_cited import CreateCitationVizPipeline
 
 __all__ = [
     "CitationPipeline",
     "CitationQAPipeline",
+    "CreateCitationVizPipeline",
 ]
